4fd21808c0e01fd5034a455c8babf84a43a1457a
unknown <spenceaw@gmail.com>
2021-02-23 15:18:43 -0500

    finished reverseString

--- reverseString.js
+++ reverseString.js
@@ -1,10 +1,20 @@
 let str = 'hello there bagel';
 
 const revStr = (str) => {
-  let arr = str.split('');
-  console.log(arr);
-  console.log(str);
-  return str;
+  return str.split('').reverse().join('');
 };
 
-revStr(str);
+const revStr1 = (str) => {
+  const arr = str.split('');
+  let x = arr.length / 2;
+  for (let i = 0; i < x; i++) {
+    let end = arr.length - i - 1; // second index to switch with first
+    let secondChar = arr[end]; // character at second index
+    let temp = arr[i]; // save first character
+    arr[i] = secondChar; // reassign first character
+    arr[end] = temp; // reassign second character
+  }
+  return arr.join('');
+};
+
+console.log(revStr1(str));
